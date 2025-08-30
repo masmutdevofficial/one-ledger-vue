31ba0b113dd5d276aeb7fb54150e91f71d170d62
Masmut Official <masmutofficial@gmail.com>
2025-08-30 13:28:39 +0700

    update

--- src/components/dashboard/SliderDashboard.vue
+++ src/components/dashboard/SliderDashboard.vue
@@ -2,22 +2,24 @@
   <Swiper
     :modules="[Autoplay, Pagination]"
     :loop="true"
-    :autoplay="{ delay: 4000, disableOnInteraction: false }"
-    :pagination="{ clickable: true }"
+    :autoplay="{ delay: 4000 }"
     class="rounded-2xl overflow-hidden mb-6"
   >
-    <SwiperSlide v-for="(src, i) in slideImages" :key="i">
-      <img :src="src" :alt="`Slide ${i + 1}`" class="block w-full h-auto" />
+    <SwiperSlide v-for="(slide, index) in slides" :key="index">
+      <div class="flex justify-between items-center bg-white p-5">
+        <div class="flex flex-col">
+          <div class="text-[10px] text-[#9ca3af] uppercase">TRADERS LEAGUE SEASON 2:</div>
+          <div class="text-black font-semibold text-[14px] mt-1">SHARE $6M IN REWARDS!</div>
+        </div>
+        <img src="/img/fire.png" class="w-6 h-6" alt="" />
+      </div>
     </SwiperSlide>
   </Swiper>
 </template>
 
 <script setup lang="ts">
-import 'swiper/css'
-import 'swiper/css/pagination'
-
 import { Swiper, SwiperSlide } from 'swiper/vue'
 import { Autoplay, Pagination } from 'swiper/modules'
 
-const slideImages = ['/img/slide-banner-1.png', '/img/slide-banner-2.png']
+const slides = [1, 2, 3]
 </script>
